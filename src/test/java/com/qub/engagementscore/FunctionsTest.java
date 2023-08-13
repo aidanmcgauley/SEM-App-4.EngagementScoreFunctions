20a5543836460900692b86d3918027b25e52651c
Aidan McGauley <40058902@ads.qub.ac.uk>
2023-08-13 19:24:19 +0100

    Fixed testing

--- src/test/java/com/qub/engagementscore/FunctionsTest.java
+++ src/test/java/com/qub/engagementscore/FunctionsTest.java
@@ -1,11 +1,8 @@
 package com.qub.engagementscore;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
 
